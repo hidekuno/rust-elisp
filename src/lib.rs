ab19210aeb4d038ab9757cbeb4322682fc0d2857
hidekuno <hidekuno@gmail.com>
2019-03-26 21:05:41 +0900

    テストコード追加

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,8 @@ pub mod lisp;
 mod tests {
     use super::*;
 
-    fn calc_int(program: &str) -> String {
+    #[allow(dead_code)]
+    fn do_lisp(program: &str) -> String {
         let mut env = lisp::SimpleEnv::new();
         match lisp::do_core_logic(program.to_string(), &mut env) {
             Ok(v) => {
@@ -17,28 +18,79 @@ mod tests {
             }
         }
     }
+    #[allow(dead_code)]
+    fn do_lisp_env(program: &str, env: &mut lisp::SimpleEnv) -> String {
+        match lisp::do_core_logic(program.to_string(), env) {
+            Ok(v) => {
+                return v.value_string();
+            }
+            _ => {
+                return String::from("");
+            }
+        }
+    }
     #[test]
     fn plus() {
-        assert!(calc_int("(+ 1 2)") == "3".to_string());
+        assert!(do_lisp("(+ 1 2)") == "3".to_string());
     }
     #[test]
     fn minus() {
-        assert!(calc_int("(- 6 1)") == "5".to_string());
+        assert!(do_lisp("(- 6 1)") == "5".to_string());
     }
     #[test]
     fn multi() {
-        assert!(calc_int("(* 3 6)") == "18".to_string());
+        assert!(do_lisp("(* 3 6)") == "18".to_string());
     }
     #[test]
     fn div() {
-        assert!(calc_int("(/ 9 3)") == "3".to_string());
+        assert!(do_lisp("(/ 9 3)") == "3".to_string());
     }
     #[test]
     fn eq() {
-        assert!(calc_int("(= 5 5)") == "#t".to_string());
+        assert!(do_lisp("(= 5 5)") == "#t".to_string());
     }
     #[test]
     fn not_eq() {
-        assert!(calc_int("(= 5 6)") == "#f".to_string());
+        assert!(do_lisp("(= 5 6)") == "#f".to_string());
+    }
+    #[test]
+    fn than() {
+        assert!(do_lisp("(> 6 5)") == "#t".to_string());
+    }
+    #[test]
+    fn not_than() {
+        assert!(do_lisp("(> 6 6)") == "#f".to_string());
+    }
+    #[test]
+    fn less() {
+        assert!(do_lisp("(< 5 6)") == "#t".to_string());
+    }
+    #[test]
+    fn not_less() {
+        assert!(do_lisp("(> 6 6)") == "#f".to_string());
+    }
+    #[test]
+    fn than_eq() {
+        assert!(do_lisp("(>= 6 6)") == "#t".to_string());
+        assert!(do_lisp("(>= 6 5)") == "#t".to_string());
+    }
+    #[test]
+    fn not_than_eq() {
+        assert!(do_lisp("(>= 5 6)") == "#f".to_string());
+    }
+    #[test]
+    fn less_eq() {
+        assert!(do_lisp("(<= 6 6)") == "#t".to_string());
+        assert!(do_lisp("(<= 5 6)") == "#t".to_string());
+    }
+    #[test]
+    fn not_less_eq() {
+        assert!(do_lisp("(<= 6 5)") == "#f".to_string());
+    }
+    #[test]
+    fn define() {
+        let mut env = lisp::SimpleEnv::new();
+        do_lisp_env("(define a 100)", &mut env);
+        assert!(do_lisp_env("a", &mut env) == "100".to_string());
     }
 }
