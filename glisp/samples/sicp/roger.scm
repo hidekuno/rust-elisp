fd7bef819e86d26470b860fc40dc829409e4f937
hidekuno <hidekuno@gmail.com>
2019-05-17 16:08:38 +0900

    lispサンプルプログラムのバグ修正

--- glisp/samples/sicp/roger.scm
+++ glisp/samples/sicp/roger.scm
@@ -1,13 +1,13 @@
-(define gframe (make-frame (make-vect 0 0) (make-vect 540 0) (make-vect 0 540)))
+(define gframe (make-frame (make-vect 0.0 0.0) (make-vect 540.0 0.0) (make-vect 0.0 540.0)))
 (define roger
   (let ((filename "/home/kunohi/rust-elisp/glisp/samples/sicp/sicp.png"))
     (lambda (f)
       (draw-image filename 
                   (list 
-                   (/ (xcor-vect (edge1-frame frame)) 180)
-                   (/ (ycor-vect (edge1-frame frame)) 180)
-                   (/ (xcor-vect (edge2-frame frame)) 180)
-                   (/ (ycor-vect (edge2-frame frame)) 180)
-                   (xcor-vect (origin-frame frame))
-                   (ycor-vect (origin-frame frame)))))))
+                   (/ (xcor-vect (edge1-frame f)) 180.0)
+                   (/ (ycor-vect (edge1-frame f)) 180.0)
+                   (/ (xcor-vect (edge2-frame f)) 180.0)
+                   (/ (ycor-vect (edge2-frame f)) 180.0)
+                   (xcor-vect (origin-frame f))
+                   (ycor-vect (origin-frame f)))))))
 ;;((square-limit roger 1) gframe)
