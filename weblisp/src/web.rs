ce2b0dbe9aeff228c1cd98a489ff6e0561b1322f
hidekuno <hidekuno@gmail.com>
2019-07-23 17:15:52 +0900

    Webサーバの処理を大幅に見直した

--- weblisp/src/web.rs
+++ weblisp/src/web.rs
@@ -3,7 +3,7 @@
    This is prototype program mini scheme subset what porting from go-scheme.
 
    hidekuno@gmail.com
-   ex) curl -v 'http://127.0.0.1:9000/lisp' --get --data-urlencode 'expr=(define #\a 100)'
+   ex) curl -v 'http://127.0.0.1:9000/lisp' --get --data-urlencode 'expr=(define a 100)'
 */
 extern crate elisp;
 use elisp::lisp;
@@ -12,6 +12,7 @@ use chrono::Utc;
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::net::Shutdown;
 use std::net::TcpStream;
 
 const PROTOCOL: &'static str = "HTTP/1.1";
@@ -24,22 +25,33 @@ const RESPONSE_500: &'static str = "500 Internal Server Error";
 struct MimeType(&'static str, &'static str);
 const MIME_PLAIN: MimeType = MimeType("txt", "text/plain");
 const MIME_HTML: MimeType = MimeType("html", "text/html");
+const MIME_PNG: MimeType = MimeType("png", "image/png");
 
 macro_rules! http_output {
     ($s: expr, $v: expr) => {
-        $s.write($v.as_bytes()).unwrap();
-        $s.write(CRLF.as_bytes()).unwrap();
+        $s.write($v.as_bytes())?;
+        $s.write(CRLF.as_bytes())?;
+    };
+}
+macro_rules! error_404 {
+    () => {
+        (
+            RESPONSE_404,
+            String::from("Not Found\r\n").into_bytes(),
+            MIME_PLAIN.1,
+        )
     };
 }
 macro_rules! error_500 {
     () => {
         (
             RESPONSE_500,
-            String::from("Internal Server Error\n"),
+            String::from("Internal Server Error\n").into_bytes(),
             MIME_PLAIN.1,
         )
     };
 }
+
 pub struct Request {
     method: String,
     resource: String,
@@ -58,22 +70,62 @@ impl Request {
 }
 pub fn handle_connection(mut stream: TcpStream, env: lisp::Environment) {
     let mut buffer = [0; 1024];
-    stream.read(&mut buffer).unwrap();
-
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(n) => {
+                if n > 0 {
+                    break;
+                }
+                if n == 0 {
+                    match stream.shutdown(Shutdown::Both) {
+                        Ok(_) => {}
+                        Err(e) => println!("{:?}", e),
+                    }
+                    return;
+                }
+            }
+            Err(e) => {
+                println!("{:?}", e);
+                return;
+            }
+        }
+    }
+    match core_proc(stream, env, &buffer) {
+        Ok(_) => {}
+        Err(ref e) => println!("{:?}", e),
+    }
+}
+fn core_proc(
+    mut stream: TcpStream,
+    env: lisp::Environment,
+    buffer: &[u8],
+) -> Result<(), Box<std::error::Error>> {
     let (status_line, contents, mime) = dispatch(&buffer, env);
+    println!("{}", status_line);
     http_output!(stream, format!("{} {}", PROTOCOL, status_line));
 
-    write_response_header(&mut stream, mime);
-    stream.write(CRLF.as_bytes()).unwrap();
-    stream.write(contents.as_bytes()).unwrap();
-    stream.flush().unwrap();
+    let date = Utc::now()
+        .format("Date: %a, %d %h %Y %H:%M:%S GMT")
+        .to_string();
+    http_output!(stream, date);
+    http_output!(stream, format!("Content-type: {}", mime));
+    http_output!(stream, format!("Content-length: {}", contents.len()));
+    let header: [&'static str; 2] = ["Server: Rust eLisp", "Connection: closed"];
+    for h in &header {
+        http_output!(stream, h);
+    }
+
+    stream.write(CRLF.as_bytes())?;
+    stream.write(contents.as_slice())?;
+    stream.flush()?;
+    Ok(())
 }
-fn dispatch(buffer: &[u8], mut env: lisp::Environment) -> (&'static str, String, &'static str) {
+fn dispatch(buffer: &[u8], mut env: lisp::Environment) -> (&'static str, Vec<u8>, &'static str) {
     let r = parse_request(buffer);
     let lisp = "/lisp?expr=";
 
     return if r.resource == "/" {
-        textfile("index.html", MIME_HTML.0)
+        static_contents("index.html")
     } else if r.resource.starts_with(lisp) {
         let (_, expr) = r.resource.split_at(lisp.len());
 
@@ -82,14 +134,9 @@ fn dispatch(buffer: &[u8], mut env: lisp::Environment) -> (&'static str, String,
             Err(e) => e.get_code(),
         };
         result.push_str("\n");
-        (RESPONSE_200, result, MIME_PLAIN.1)
-    } else if r
-        .resource
-        .ends_with((".".to_owned() + MIME_HTML.0).as_str())
-    {
-        textfile(r.get_resource(), MIME_HTML.0)
+        (RESPONSE_200, result.into_bytes(), MIME_PLAIN.1)
     } else {
-        textfile(r.get_resource(), MIME_HTML.0)
+        static_contents(r.get_resource())
     };
 }
 fn parse_request(buffer: &[u8]) -> Request {
@@ -131,7 +178,9 @@ fn urldecode(s: &str) -> String {
     }
     r
 }
-fn textfile<'a>(filename: &str, mime: &'a str) -> (&'a str, String, &'a str) {
+fn static_contents<'a>(filename: &str) -> (&'a str, Vec<u8>, &'a str) {
+    let mut vec = Vec::new();
+
     let mut path = match env::current_dir() {
         Ok(f) => f,
         Err(_) => return error_500!(),
@@ -143,7 +192,7 @@ fn textfile<'a>(filename: &str, mime: &'a str) -> (&'a str, String, &'a str) {
         path.push(s);
     }
     if false == path.as_path().exists() {
-        return (RESPONSE_404, String::from("Not Found\r\n"), MIME_PLAIN.1);
+        return error_404!();
     }
     let mut file = match File::open(path) {
         Ok(file) => file,
@@ -154,23 +203,21 @@ fn textfile<'a>(filename: &str, mime: &'a str) -> (&'a str, String, &'a str) {
         Err(_) => return error_500!(),
     };
     if true == meta.is_dir() {
-        return textfile(&(filename.to_owned() + "/index.html"), mime);
+        return static_contents(&(filename.to_owned() + "/index.html"));
     }
-
-    let mut contents = String::new();
-    return match file.read_to_string(&mut contents) {
-        Ok(_) => (RESPONSE_200, contents, mime),
+    return match file.read_to_end(&mut vec) {
+        Ok(_) => (RESPONSE_200, vec, get_mime(filename)),
         Err(_) => error_500!(),
     };
 }
-fn write_response_header(stream: &mut TcpStream, mime: &str) {
-    let date = Utc::now()
-        .format("Date: %a, %d %h %Y %H:%M:%S GMT")
-        .to_string();
-    http_output!(stream, date);
-    http_output!(stream, format!("Content-type: {}", mime));
-    let header: [&'static str; 2] = ["Server: Rust eLisp", "Connection: keep-alive"];
-    for h in &header {
-        http_output!(stream, h);
-    }
+fn get_mime(filename: &str) -> &'static str {
+    return if filename.ends_with((".".to_owned() + MIME_HTML.0).as_str()) {
+        MIME_HTML.1
+    } else if filename.ends_with((".".to_owned() + MIME_PLAIN.0).as_str()) {
+        MIME_PLAIN.1
+    } else if filename.ends_with((".".to_owned() + MIME_PNG.0).as_str()) {
+        MIME_PNG.1
+    } else {
+        MIME_PLAIN.1
+    };
 }
