318d4c4d4058a13bc95d4b6f351d2ca29c47ffd3
hideki kuno <kunohi@firstserver.co.jp>
2019-07-30 11:28:45 +0900

    Webのコード見直し及びテストコードの見直し

--- weblisp/src/web.rs
+++ weblisp/src/web.rs
@@ -11,6 +11,8 @@ use elisp::lisp;
 
 use chrono::Utc;
 use std::env;
+use std::error::Error;
+use std::fmt;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
@@ -21,6 +23,7 @@ const PROTOCOL: &'static str = "HTTP/1.1";
 const CRLF: &'static str = "\r\n";
 
 const RESPONSE_200: &'static str = "200 OK";
+const RESPONSE_400: &'static str = "400 Bad Request";
 const RESPONSE_404: &'static str = "404 Not Found";
 const RESPONSE_405: &'static str = "405 Method Not Allowed";
 const RESPONSE_500: &'static str = "500 Internal Server Error";
@@ -30,9 +33,28 @@ const MIME_PLAIN: MimeType = MimeType("txt", "text/plain");
 const MIME_HTML: MimeType = MimeType("html", "text/html");
 const MIME_PNG: MimeType = MimeType("png", "image/png");
 const DEFALUT_MIME: &'static str = "application/octet-stream";
+const CGI_EXT: &'static str = ".cgi";
 
-const LISP: &'static str = "/lisp?expr=";
+const LISP: &'static str = "/lisp";
+const LISP_PARAMNAME: &'static str = "expr=";
 
+#[derive(Debug)]
+struct UriParseError {
+    pub code: &'static str,
+}
+impl fmt::Display for UriParseError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.code)
+    }
+}
+impl Error for UriParseError {
+    fn description(&self) -> &str {
+        self.code
+    }
+    fn cause(&self) -> Option<&Error> {
+        None
+    }
+}
 macro_rules! print_error {
     ($f: expr, $e: expr) => {
         println!("{} fault: {:?}", $f, $e)
@@ -44,29 +66,11 @@ macro_rules! http_write {
         $s.write(CRLF.as_bytes())?;
     };
 }
-macro_rules! error_404 {
-    () => {
+macro_rules! http_error {
+    ($c: expr) => {
         (
-            RESPONSE_404,
-            Contents::String(String::from(&RESPONSE_404[4..]) + CRLF),
-            MIME_PLAIN.1,
-        )
-    };
-}
-macro_rules! error_405 {
-    () => {
-        (
-            RESPONSE_405,
-            Contents::String(String::from(&RESPONSE_405[4..]) + CRLF),
-            MIME_PLAIN.1,
-        )
-    };
-}
-macro_rules! error_500 {
-    () => {
-        (
-            RESPONSE_500,
-            Contents::String(String::from(&RESPONSE_500[4..]) + CRLF),
+            $c,
+            Contents::String(String::from(&$c[4..]) + CRLF),
             MIME_PLAIN.1,
         )
     };
@@ -75,6 +79,7 @@ pub struct Request {
     method: String,
     resource: String,
     protocol: String,
+    parameter: String,
 }
 impl Request {
     pub fn get_method(&self) -> &String {
@@ -85,6 +90,9 @@ impl Request {
     }
     pub fn get_protocol(&self) -> &String {
         &self.protocol
+    }
+    pub fn get_parameter(&self) -> &String {
+        &self.parameter
     }
 }
 enum Contents {
@@ -107,7 +115,10 @@ impl Contents {
     fn len(&self) -> usize {
         match self {
             Contents::String(v) => v.len(),
-            Contents::File(v) => v.metadata().unwrap().len() as usize,
+            Contents::File(v) => match v.metadata() {
+                Ok(v) => v.len() as usize,
+                Err(_) => 0 as usize,
+            },
         }
     }
 }
@@ -118,7 +129,7 @@ pub fn handle_connection(mut stream: TcpStream, env: lisp::Environment) {
 
     let mut buffer = [0; 1024];
     // read() is Not Good.(because it's not detected EOF)
-    // I try read_to_end() and read_exact(), But it's Ng
+    // I try read_to_end() and read_exact(), But it was NG
     match stream.read(&mut buffer) {
         Ok(_) => {}
         Err(e) => {
@@ -135,7 +146,7 @@ fn core_proc(
     mut stream: TcpStream,
     env: lisp::Environment,
     buffer: &[u8],
-) -> Result<(), Box<std::error::Error>> {
+) -> Result<(), Box<Error>> {
     let (status_line, mut contents, mime) = dispatch(&buffer, env);
     println!("{}", status_line);
     http_write!(stream, format!("{} {}", PROTOCOL, status_line));
@@ -157,14 +168,17 @@ fn core_proc(
     Ok(())
 }
 fn dispatch(buffer: &[u8], mut env: lisp::Environment) -> (&'static str, Contents, &'static str) {
-    let r = parse_request(buffer);
+    let r = match parse_request(buffer) {
+        Ok(r) => r,
+        Err(_) => return http_error!(RESPONSE_400),
+    };
     if r.get_method() != "GET" {
-        return error_405!();
+        return http_error!(RESPONSE_405);
     }
     return if r.get_resource() == "/" {
         static_contents("index.html")
     } else if r.get_resource().starts_with(LISP) {
-        let (_, expr) = r.get_resource().split_at(LISP.len());
+        let (_, expr) = r.get_parameter().split_at(LISP_PARAMNAME.len());
 
         let mut result = match lisp::do_core_logic(&expr.to_string(), &mut env) {
             Ok(r) => r.to_string(),
@@ -172,12 +186,14 @@ fn dispatch(buffer: &[u8], mut env: lisp::Environment) -> (&'static str, Content
         };
         result.push_str(CRLF);
         (RESPONSE_200, Contents::String(result), MIME_PLAIN.1)
+    } else if r.get_resource().ends_with(CGI_EXT) {
+        do_cgi(&r)
     } else {
         static_contents(r.get_resource())
     };
 }
-fn parse_request(buffer: &[u8]) -> Request {
-    let mut lines = std::str::from_utf8(buffer).unwrap().lines();
+fn parse_request(buffer: &[u8]) -> Result<Request, Box<Error>> {
+    let mut lines = std::str::from_utf8(buffer)?.lines();
     let mut requst: [&str; 128] = [""; 128];
 
     if let Some(r) = lines.next() {
@@ -185,22 +201,42 @@ fn parse_request(buffer: &[u8]) -> Request {
         for (i, s) in r.split_whitespace().into_iter().enumerate() {
             requst[i] = s;
         }
+    } else {
+        return Err(Box::new(UriParseError { code: "E2001" }));
     }
-    Request {
+    let iter = urldecode(requst[1])?;
+    let mut iter = iter.split('?').into_iter();
+
+    let url = if let Some(s) = iter.next() {
+        s
+    } else {
+        return Err(Box::new(UriParseError { code: "E2001" }));
+    };
+    let parameter = if let Some(s) = iter.next() { s } else { "" };
+
+    Ok(Request {
         method: String::from(requst[0]),
-        resource: urldecode(requst[1]),
+        resource: String::from(url),
         protocol: String::from(requst[2]),
-    }
+        parameter: String::from(parameter),
+    })
 }
-fn urldecode(s: &str) -> String {
+fn urldecode(s: &str) -> Result<String, Box<Error>> {
     enum PercentState {
         Init,
         First,
         Second,
     }
+    enum ByteMode {
+        ASCII,
+        Jpn,
+    }
     let mut r = String::new();
-    let mut e: [u8; 2] = [0; 2];
+    let mut en: [u8; 2] = [0; 2];
+    let mut ja: [u8; 3] = [0; 3];
+    let mut ja_cnt = 0;
     let mut state = PercentState::Init;
+    let mut mode = ByteMode::ASCII;
 
     for b in s.bytes() {
         let c = b as char;
@@ -210,31 +246,43 @@ fn urldecode(s: &str) -> String {
                 _ => r.push(c),
             },
             PercentState::First => {
+                en[0] = b;
+                state = PercentState::Second;
+
                 // not support multi byte
-                match b {
-                    0x30...0x37 => {
-                        e[0] = b;
-                        state = PercentState::Second;
-                    }
-                    _ => state = PercentState::Init,
+                mode = match b {
+                    0x30...0x37 => ByteMode::ASCII,
+                    _ => ByteMode::Jpn,
                 }
             }
             PercentState::Second => {
-                e[1] = b;
+                en[1] = b;
                 state = PercentState::Init;
-                match u8::from_str_radix(std::str::from_utf8(&e).unwrap(), 16) {
-                    Ok(v) => r.push(v as char),
-                    Err(e) => print_error!("u8::from_str_radix hex", e),
+                match mode {
+                    ByteMode::ASCII => {
+                        let v = u8::from_str_radix(std::str::from_utf8(&en)?, 16)?;
+                        r.push(v as char);
+                    }
+                    ByteMode::Jpn => {
+                        ja[ja_cnt] = u8::from_str_radix(std::str::from_utf8(&en)?, 16)?;
+
+                        ja_cnt += 1;
+                        if ja_cnt == 3 {
+                            mode = ByteMode::ASCII;
+                            ja_cnt = 0;
+                            r.push_str(std::str::from_utf8(&ja)?);
+                        }
+                    }
                 }
             }
         }
     }
-    r
+    Ok(r)
 }
 fn static_contents<'a>(filename: &str) -> (&'a str, Contents, &'a str) {
     let mut path = match env::current_dir() {
         Ok(f) => f,
-        Err(_) => return error_500!(),
+        Err(_) => return http_error!(RESPONSE_500),
     };
     for s in filename.split('/') {
         if s == "" {
@@ -243,15 +291,15 @@ fn static_contents<'a>(filename: &str) -> (&'a str, Contents, &'a str) {
         path.push(s);
     }
     if false == path.as_path().exists() {
-        return error_404!();
+        return http_error!(RESPONSE_404);
     }
     let file = match File::open(path) {
         Ok(file) => file,
-        Err(_) => return error_500!(),
+        Err(_) => return http_error!(RESPONSE_500),
     };
     let meta = match file.metadata() {
         Ok(meta) => meta,
-        Err(_) => return error_500!(),
+        Err(_) => return http_error!(RESPONSE_500),
     };
     if true == meta.is_dir() {
         return static_contents(&(filename.to_owned() + "/index.html"));
@@ -272,4 +320,7 @@ fn get_mime(filename: &str) -> &'static str {
     } else {
         DEFALUT_MIME
     };
+}
+fn do_cgi(r: &Request) -> (&'static str, Contents, &'static str) {
+    http_error!(RESPONSE_400)
 }
