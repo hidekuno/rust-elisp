59417f9a5983e9b1d4618c3dc4ec3432c5739af2
hidekuno <hidekuno@gmail.com>
2019-10-04 11:55:29 +0900

    図形プログラムを新規リポジトリに移行した

--- glisp/samples/fractal/common.scm
+++
@@ -1,8 +0,0 @@
-;
-; this is a common parts for sample program
-;
-; hidekuno@gmail.com
-;
-(define pi (*(atan 1)4))
-(define (cs angle)(cos (/(* pi angle)180)))
-(define (sn angle)(sin (/(* pi angle)180)))
