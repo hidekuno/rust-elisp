f3cf7e6bec8eb7e9f94d63800890f3e9dfa66b5e
hidekuno <hidekuno@gmail.com>
2019-10-10 09:13:18 +0900

    string-length関数追加と結合テスト追加

---
+++ elisp/samples/easy-hash.scm
@@ -0,0 +1,14 @@
+;
+; this is a sample program
+;
+; hidekuno@gmail.com
+(define tekito-prime 1021)
+(define (easy-hash s)
+  (let loop ((x (map (lambda (n) (char->integer n))(string->list s)))
+             (h 0)
+             (u 0))
+    (cond ((null? x) (modulo (+ u h) tekito-prime))
+          (else
+           (loop (cdr x)
+                 (modulo (+ (car x) (* h (string-length s))) tekito-prime)
+                 (- 8 (modulo (+ (car x) (* u (string-length s))) 8)))))))
