21e21f800dfea6819bc5d94f5f8321b7d03cd115
hidekuno <hidekuno@gmail.com>
2020-03-27 14:26:28 +0900

    wasmのUIを改善

--- wasmlisp/index.js
+++ wasmlisp/index.js
@@ -1,26 +1,44 @@
+/*
+   Rust study program.
+   This is prototype program mini scheme subset what porting from go-scheme.
+
+   hidekuno@gmail.com
+*/
 import('./pkg').catch(console.error);
 
-var editor = ace.edit("editor");
-editor.$blockScrolling = Infinity;
-editor.setOptions({
-  enableBasicAutocompletion: true,
-  enableSnippets: true,
-  enableLiveAutocompletion: true
-});
+(() => {
 
-editor.setTheme("ace/theme/textmate");
-editor.getSession().setMode("ace/mode/scheme");
-editor.setFontSize(12)
+    let editor = ace.edit("editor");
+    editor.$blockScrolling = Infinity;
+    editor.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true
+    });
+    editor.setTheme("ace/theme/textmate");
+    editor.getSession().setMode("ace/mode/scheme");
+    editor.setFontSize(12);
 
-var codearea = document.getElementById("codearea");
-var eval_button = document.getElementById("eval");
+    let codearea = document.getElementById("codearea");
+    let evalButton = document.getElementById("eval");
 
-eval_button.onmousedown = function() {
-  codearea.value = editor.getSession().getValue();
-}
+    evalButton.onmousedown = () => {
+        codearea.value = editor.getSession().getValue();
+    };
 
-eval_button.onkeydown = function(e) {
-  if (event.keyCode == 32) {
-    codearea.value = editor.getSession().getValue();
-  }
-}
+    evalButton.onkeydown = () => {
+        if (event.keyCode == 32) {
+            codearea.value = editor.getSession().getValue();
+        }
+    };
+
+    document.getElementById("sicp").onclick = () => {
+        editor.setValue('(load-url "z-learning/wasm-sicp.scm")', -1);
+    };
+    document.getElementById("demo").onclick = () => {
+        editor.setValue('(demo)', -1);
+    };
+    document.getElementById("album").onclick = () => {
+        editor.setValue('((below(beside rv ps)(beside sd am))(make-image-frame-rectangle "am" 2.2 2.2))', -1);
+    };
+})();
--- wasmlisp/webpack.config.js
+++ wasmlisp/webpack.config.js
@@ -1,14 +1,20 @@
+/*
+   Rust study program.
+   This is prototype program mini scheme subset what porting from go-scheme.
+
+   hidekuno@gmail.com
+*/
 const path = require('path');
 
 module.exports = {
-  entry: "./index.js",
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.js",
-  },
-  devServer: {
-    host: '0.0.0.0',
-    port: 8080
-  },
-  mode: "development"
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "index.js",
+    },
+    devServer: {
+        host: '0.0.0.0',
+        port: 8080
+    },
+    mode: "development"
 };
