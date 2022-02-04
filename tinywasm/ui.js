c1e18a651a913c331edbaa03fdbcbcefb13b1d6c
hideki kuno <hidekuno@gmail.com>
2022-02-04 12:29:22 +0900

    tinywasmにコンテンツを追加した

---
+++ tinywasm/ui.js
@@ -0,0 +1,89 @@
+/*
+   Rust study program.
+   This is prototype program mini scheme subset what porting from go-scheme.
+
+   hidekuno@gmail.com
+*/
+
+const PRIME_CODE =`(define (prime l)
+  (if (> (car l)(sqrt (last l))) l
+      (cons (car l)(prime (filter (lambda (n) (not (= 0 (modulo n (car l))))) (cdr l))))))
+
+(prime (iota 30 2))`;
+
+const PERM_CODE = `(define (perm l n)
+  (if (>= 0 n) (list (list))
+      (reduce (lambda (a b)(append a b))(list)
+              (map (lambda (x) (map (lambda (p) (cons x p)) (perm (delete x l)(- n 1)))) l))))
+
+(perm '(a b c) 2)`;
+
+const COMB_CODE = `(define (comb l n)
+  (if (null? l) l
+      (if (= n 1) (map (lambda (n) (list n)) l)
+          (append (map (lambda (p) (cons (car l) p)) (comb (cdr l)(- n 1))) (comb (cdr l) n)))))
+
+(comb '(a b c) 2)`;
+
+const QSORT_CODE = `(define (qsort l pred)
+  (if (null? l) l
+      (append (qsort (filter (lambda (n) (pred n (car l))) (cdr l)) pred)
+              (cons (car l) (qsort (filter (lambda (n) (not (pred n (car l))))(cdr l)) pred)))))
+
+(define test-list (list 36 14 19 2 8 7 6 27 0 9 3))
+(qsort test-list (lambda (a b)(< a b)))`;
+
+const WEB_FONT = "<i class='fa fa-spinner fa-spin fa-5x fa-fw'></i><br><br>";
+
+const WAIT_MESSAGE = "Please wait until the alert dialog is displayed.";
+
+function addLoading() {
+    let ua = window.navigator.userAgent.toLowerCase();
+
+    let msg  = (ua.indexOf('firefox') != -1)
+        ?"<div class='loadingMsg'>" + WEB_FONT + WAIT_MESSAGE +"</div>"
+        :"<div class='loadingMsg'>" + WAIT_MESSAGE +"</div>";
+
+    if ($("#loading").length == 0) {
+        $("body").append("<div id='loading'>" + msg + "</div>");
+        if (ua.indexOf('firefox') == -1) {
+            $('.loadingMsg').css('background', "url('/loading.gif') center center no-repeat");
+        }
+    }
+}
+(() => {
+    let editor = ace.edit("editor");
+    editor.$blockScrolling = Infinity;
+    editor.setOptions({
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true
+    });
+    editor.setTheme("ace/theme/textmate");
+    editor.getSession().setMode("ace/mode/scheme");
+    editor.setFontSize(12);
+
+    let codeArea = document.getElementById("codearea");
+    let evalButton = document.getElementById("eval");
+
+    evalButton.onmousedown = () => {
+        // addLoading();
+        codeArea.value = editor.getSession().getValue();
+    };
+    evalButton.onclick = () => {
+        let v = do_scheme(codeArea.value);
+        alert(v);
+    };
+    document.getElementById("prime").onclick = () => {
+        editor.setValue(PRIME_CODE, -1);
+    };
+    document.getElementById("permutations").onclick = () => {
+        editor.setValue(PERM_CODE, -1);
+    };
+    document.getElementById("combinations").onclick = () => {
+        editor.setValue(COMB_CODE, -1);
+    };
+    document.getElementById("quicksort").onclick = () => {
+        editor.setValue(QSORT_CODE, -1);
+    };
+})();
